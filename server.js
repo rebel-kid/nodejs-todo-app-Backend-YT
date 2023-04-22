1af248f2f843ae314a8845d8a0cf872ed6887a19
shashanksingh0617 <shashank.singh.0617@gmail.com>
2023-04-22 12:28:35 +0530

    CORS Parsing

--- server.js
+++ server.js
@@ -6,4 +6,5 @@ connectDB();
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is working on port: ${process.env.PORT} in ${process.env.NODE_ENV} mode`);           
+    console.log(`FRONTEND_URL is ${process.env.FRONTEND_URL}`);
 })
