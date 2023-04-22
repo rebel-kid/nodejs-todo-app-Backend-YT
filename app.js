7a39dab6f49a58f7f6f8f00326d3df6f2609f93d
shashanksingh0617 <shashank.singh.0617@gmail.com>
2023-04-22 12:22:29 +0530

    WIP

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cookieParser()); //need cookie parser to fetch user id from token genera
 //routes
 //adding workaround for cors issue
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, application/json');
      // Set to true if you need the website to include cookies in the requests sent
