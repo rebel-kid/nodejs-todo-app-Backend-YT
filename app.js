be4d2d3e9abc4326fc2a4b9a74a1f9c0f33a04c7
shashanksingh0617 <shashank.singh.0617@gmail.com>
2023-04-22 10:51:59 +0530

    WIIP

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.get("/", (req, res) => {
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, application/json');
     next();
   });
 
