a464019e174b8510db202c6346d33a3aada0e72e
shashanksingh0617 <shashank.singh.0617@gmail.com>
2023-04-22 12:16:36 +0530

    CORS

--- app.js
+++ app.js
@@ -16,6 +16,18 @@ config({
 app.use(express.json());
 app.use(cookieParser()); //need cookie parser to fetch user id from token generated, as user will be logged in, cookie will be there
 //routes
+//adding workaround for cors issue
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, application/json');
+     // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+  });
+//use middleware before routes
 app.use("/api/v1/users",userRouter);
 app.use("/api/v1/tasks",taskRouter);
 // app.use(cors({
@@ -29,13 +41,6 @@ app.get("/", (req, res) => {
     res.send("Nice")
     // console.log(process.env.FRONTEND_URL);
 })
-//adding workaround for cors issue
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, application/json');
-    next();
-  });
 
 
 //MIDDLEWARE FOR ERROR
